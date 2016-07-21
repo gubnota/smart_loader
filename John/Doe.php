cd2d26f045166e824f5b391894ce185d47b1597a
gubnota <me@gubnota.ru>
2016-07-21 10:34:01 +0800

    minor change in example

--- John/Doe.php
+++ John/Doe.php
@@ -4,11 +4,15 @@ namespace John;
 class Doe{
 
 	public function write_message_to($name,$message = '<message text to put in>'){
-		print "---------------------------\n";
-		print "Hello $name,\n";
-		print "$message\n";
+		print "----------------------------\n";
+		print str_pad("Hello $name,",28,' '). "|\n";
+		$strs = explode("\n", $message);
+		foreach ($strs as $key => $str) {
+		print " " .str_pad(rtrim($str),27,' '). "|\n";
+		}
 		$self_name = str_replace('\\',' ',__CLASS__);
-		print "Regards,\n".$self_name."\n";
-		print "---------------------------\n\n\n";
+		print " " .str_pad("Regards,  ",27,' ', STR_PAD_LEFT). "|\n";
+		print " " .str_pad($self_name,27,' ', STR_PAD_LEFT). "|\n";
+		print "----------------------------\n\n\n";
 	}
 }
