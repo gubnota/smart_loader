c482caeaae4628fd9ac6ef4b7d92fcdd217bd29b
gubnota <me@gubnota.ru>
2016-10-23 14:24:49 +0800

    smart_loader.php

--- init.php
+++ init.php
@@ -4,6 +4,7 @@
 */
 include(__DIR__.'/Gubnota/Smart_loader.php');
 $l = \Gubnota\Smart_loader::instance();
-//$l->place('new_place_to_autoload',__DIR__);
+$l->place('smart_loader',__DIR__);
 spl_autoload_register([$l, 'load']);
 //g::John('write_message_to','Jane','Hi');
+//to enumerate all registered directories: print_r($l->place();
