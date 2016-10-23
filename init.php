ea0dcbdc753109a06ee46d26fde388ead45b736b
gubnota <me@gubnota.ru>
2016-10-23 15:50:58 +0800

    init.php checks whether it initialized already

--- init.php
+++ init.php
@@ -2,9 +2,11 @@
 /**
  * simplier autoloader, just include this file to load gubnota/smart_loader
 */
-include(__DIR__.'/Gubnota/Smart_loader.php');
+$loaded=false;
+if (class_exists('\Gubnota\Smart_loader')) {$loaded=true;}
+if (!$loaded) include __DIR__.'/Gubnota/Smart_loader.php';
 $l = \Gubnota\Smart_loader::instance();
 $l->place('smart_loader',__DIR__);
-spl_autoload_register([$l, 'load']);
+if (!$loaded) spl_autoload_register([$l, 'load']);
 //g::John('write_message_to','Jane','Hi');
 //to enumerate all registered directories: print_r($l->place();
